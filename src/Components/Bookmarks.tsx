78e670fc4a25d1e12aa0dc48f87490f1302d29d0
Daniel Molloy <danielmolloy_6@icloud.com>
2021-08-30 15:34:56 +1000

    Bookmarks page

--- src/Components/Bookmarks.tsx
+++ src/Components/Bookmarks.tsx
@@ -1,8 +1,19 @@
 
 export const Bookmarks = () => {
   return (
-    <div>
-      Bookmarks
+    <div className="border-r w-full mr-2">
+      <div className="border-b">
+        <h2 className="text-xl font-semibold pl-2 pt-2 pb-0">Bookmarks</h2>
+        <p className="user pl-2 -pt-2 text-xs text-gray-600 mb-1.5">@dan_molloy1</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col w-1/2">
+        <h3 className="text-3xl font-bold">
+          You haven't added any Tweets to your Bookmarks yet
+        </h3>
+        <p className="text-s text-gray-600">When you do, they'll show up here.</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -7,9 +7,9 @@ export const Home = () => {
     <div id="home" className="border-r w-full mr-2">
       <div className="header border-b border-gray-200 h-16">
         <h2 className="text-xl font-semibold p-4">Home</h2>
-        <ComposeTweet />
-        <SingleTweet />
       </div>
+      <ComposeTweet />
+      <SingleTweet />
     </div>
   )
 }
