8ef4ecdabcf807d85d5be39b6c9669f2a9d4eb58
Daniel Molloy <danielmolloy_6@icloud.com>
2021-11-17 20:47:49 +0000

    uri updated

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,7 @@ import { setContext } from '@apollo/client/link/context';
 import { AUTH_TOKEN } from './constants';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: 'https://twitr-clone.herokuapp.com/'
 });
 
 const authLink = setContext((_, { headers }) => {
