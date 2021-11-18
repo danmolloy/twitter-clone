dc77d1caab5e41bb4fb0f85cd6c2300a2ed4e748
Daniel Molloy <danielmolloy_6@icloud.com>
2021-11-18 11:17:11 +0000

    sign-in from error page

--- client/src/Components/App/Error.tsx
+++ client/src/Components/App/Error.tsx
@@ -1,14 +1,11 @@
 import { GlobeIcon, RefreshIcon } from "@heroicons/react/outline"
-import { useState } from "react"
+import { AUTH_TOKEN } from '../../constants'
+
 
 export const Error = () => {
-  const [retryCount, setRetryCount] = useState(0)
 
   const handleClick = () => {
-    setRetryCount(retryCount + 1);
-    if (retryCount > 1) {
-      localStorage.clear();
-    }
+    localStorage.removeItem(AUTH_TOKEN)
     window.location.reload()
   }
 
@@ -16,9 +13,13 @@ export const Error = () => {
     <div id="error-page" className="w-full flex flex-col items-center mt-12">
       <GlobeIcon className="h-6 w-auto"/>
       <p className="text-gray-600">An error occurred. Please check your connection and try again.</p>
+      <button className="tweet-btn py-1 flex flex-row" onClick={() => window.location.reload()}>
+        <RefreshIcon className="h-6 w-auto"/>
+        <p className="font-semibold px-1">"Retry"</p>
+      </button>
       <button className="tweet-btn py-1 flex flex-row" onClick={() => handleClick()}>
         <RefreshIcon className="h-6 w-auto"/>
-        <p className="font-semibold px-1">{retryCount === 0 ? "Retry" : "Reboot"}</p>
+        <p className="font-semibold px-1">"Sign in again"</p>
       </button>
     </div>
   )
