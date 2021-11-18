3a9a6da2046c85c95513926a7bf6a4e8b67d78cc
Daniel Molloy <danielmolloy_6@icloud.com>
2021-11-18 10:57:29 +0000

    handleclick

--- client/src/Components/App/Error.tsx
+++ client/src/Components/App/Error.tsx
@@ -1,11 +1,17 @@
 import { GlobeIcon, RefreshIcon } from "@heroicons/react/outline"
 
 export const Error = () => {
+
+  const handleClick = () => {
+    alert(Object.keys(localStorage))
+    window.location.reload()
+  }
+
   return (
     <div id="error-page" className="w-full flex flex-col items-center mt-12">
       <GlobeIcon className="h-6 w-auto"/>
       <p className="text-gray-600">An error occurred. Please check your connection and try again.</p>
-      <button className="tweet-btn py-1 flex flex-row" onClick={() => window.location.reload()}>
+      <button className="tweet-btn py-1 flex flex-row" onClick={() => handleClick()}>
         <RefreshIcon className="h-6 w-auto"/>
         <p className="font-semibold px-1">Retry</p>
       </button>
